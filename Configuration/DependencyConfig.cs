7a0bca183a4a73dcf07a80b73cc5f83e506e8593
Sakib Mahmood <sakib.mahmood@brainstation-23.com>
2023-12-13 21:45:33 +0600

    add plugin from object

--- Configuration/DependencyConfig.cs
+++ Configuration/DependencyConfig.cs
@@ -4,6 +4,10 @@ using Microsoft.SemanticKernel;
 using test_generative_ai.Managers;
 using GenerativeAIChatBot.Persistence.Contexts;
 using GenerativeAIChatBot.Persistence.Repositories;
+using Microsoft.SemanticKernel.Memory;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI.TextEmbedding;
+using Microsoft.SemanticKernel.Connectors.Memory.Sqlite;
+using Microsoft.Extensions.Configuration;
 
 namespace test_generative_ai.Configuration
 {
@@ -11,17 +15,47 @@ namespace test_generative_ai.Configuration
     {
         public static IServiceCollection AddDependencies(this IServiceCollection services)
         {
+
             services.AddDbContext<StoreContext>();
 
             services.AddSingleton<Kernel>(provider =>
             {
                 var kernelBuilder = new KernelBuilder();
                 var model = "gpt-3.5-turbo";
-                var apiKey = "";
+                var apiKey = "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";
                 var orgId = "";
-                kernelBuilder.AddOpenAIChatCompletion(model, apiKey, orgId);
+                kernelBuilder.AddOpenAIChatCompletion(model, apiKey, orgId)
+                            .AddOpenAITextEmbeddingGeneration(model, apiKey, orgId);
+
                 return kernelBuilder.Build();
             });
+
+
+#pragma warning disable SKEXP0003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+            services.AddSingleton<SemanticTextMemory>(provider =>
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"))
+                .Build();
+
+                var defaultConnectionString = configuration.GetConnectionString("DefaultConnection");
+
+
+#pragma warning disable SKEXP0028 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+                IMemoryStore store = SqliteMemoryStore.ConnectAsync("C:\\BS23\\Personal\\generative-ai-chatbot\\Database\\generativeai.db").GetAwaiter().GetResult();
+#pragma warning restore SKEXP0028 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+                var model = "text-embedding-ada-002";
+                var apiKey = "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";
+
+                var embeddingGenerator = new OpenAITextEmbeddingGeneration(model, apiKey);
+
+                SemanticTextMemory textMemory = new(store, embeddingGenerator);
+
+                return textMemory;
+            });
+#pragma warning restore SKEXP0003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
 
             services.AddSingleton(new OpenAIPromptExecutionSettings
             {
--- Managers/OpenAIPromptManager.cs
+++ Managers/OpenAIPromptManager.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Memory;
+using Microsoft.SemanticKernel.Plugins.Memory;
 
 namespace test_generative_ai.Managers
 {
@@ -7,27 +9,70 @@ namespace test_generative_ai.Managers
     public class OpenAIPromptManager: IPromptManager
     {
         private readonly Kernel _kernel;
+#pragma warning disable SKEXP0003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        private readonly SemanticTextMemory _semanticTextMemory;
+#pragma warning restore SKEXP0003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
         private readonly OpenAIPromptExecutionSettings _executionSettings;
 
-        public OpenAIPromptManager(Kernel kernel, OpenAIPromptExecutionSettings executionSettings)
+#pragma warning disable SKEXP0003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        public OpenAIPromptManager(Kernel kernel, OpenAIPromptExecutionSettings executionSettings, SemanticTextMemory semanticTextMemory)
+#pragma warning restore SKEXP0003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
         {
             _kernel = kernel;
+            _semanticTextMemory = semanticTextMemory;
             _executionSettings = executionSettings;
         }
 
         public async Task<string> ChatResultFunctionAsync(KernelArguments argument)
         {
+#pragma warning disable SKEXP0003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
+#pragma warning disable SKEXP0028 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+       //     IMemoryStore store = await SqliteMemoryStore.ConnectAsync("memories.sqlite");
+#pragma warning restore SKEXP0028 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+#pragma warning restore IDE0059 // Unnecessary assignment of a value
+#pragma warning restore SKEXP0003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+#pragma warning disable SKEXP0052 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+#pragma warning restore SKEXP0052 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+           // await _semanticTextMemory.SaveInformationAsync("brain-station-23", id: "info1", text: "My name is Andrea");
+
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
+#pragma warning disable SKEXP0052 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+            //var memoryPlugin = _kernel.ImportPluginFromObject(new TextMemoryPlugin(_semanticTextMemory));
+#pragma warning restore SKEXP0052 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+#pragma warning restore IDE0059 // Unnecessary assignment of a value
+
+#pragma warning disable SKEXP0003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+            MemoryQueryResult? lookup = await _semanticTextMemory.GetAsync("brain-station-23", "info1");
+#pragma warning restore SKEXP0003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+            Console.WriteLine("Memory with key 'info1':" + lookup?.Metadata.Text ?? "ERROR: memory not found");
+
             const string skPrompt = @"
             ChatBot can have a conversation with you about any topic.
             It can give explicit instructions or say 'I don't know' if it does not have an answer.
 
             {{$history}}
-            User: {{$userInput}}
+            User: {{$input}}
             ChatBot:";
 
+#pragma warning disable SKEXP0052 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+            var memoryPlugin = _kernel.ImportPluginFromObject(new TextMemoryPlugin(_semanticTextMemory));
+#pragma warning restore SKEXP0052 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+           // var data = await _kernel.InvokeAsync(memoryPlugin["Recall"], argument);
+
             var chatFunction = _kernel.CreateFunctionFromPrompt(skPrompt, _executionSettings);
-            var answer = await chatFunction.InvokeAsync(_kernel, argument);
-            return answer.ToString();
+
+            //var textPromptResult = await _kernel.InvokeAsync(chatFunction);
+           // _kernel.Plugins.Add(memoryPlugin);
+           // var data = await memoryPlugin["Recall"].InvokeAsync(argument);
+
+               var data = await chatFunction.InvokeAsync(_kernel, argument);
+
+            return data.ToString();
         }
     }
 }
